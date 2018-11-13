d226a1dc8d1846794af4c9e85162828c1a75c341
Chris Durbin <christopher.d.durbin@nasa.gov>
2018-11-13 14:59:48 -0500

    CMR-5271: As a data provider, I want OUS to use the OPeNDAP URL from my Online Resources as the base OPeNDAP URL.

--- project.clj
+++ project.clj
@@ -15,7 +15,7 @@
        ns
        "\u001B[35m]\u001B[33m λ\u001B[m=> "))
 
-(defproject gov.nasa.earthdata/cmr-ous-plugin "0.2.2-SNAPSHOT"
+(defproject gov.nasa.earthdata/cmr-ous-plugin "0.2.3-SNAPSHOT"
   :description "A CMR services plugin that performs URL translations for subsetted GIS data"
   :url "https://github.com/cmr-exchange/cmr-ous-plugin"
   :license {
@@ -32,10 +32,10 @@
     [gov.nasa.earthdata/cmr-exchange-query "0.2.0-SNAPSHOT"]
     [gov.nasa.earthdata/cmr-http-kit "0.1.5-SNAPSHOT"]
     [gov.nasa.earthdata/cmr-jar-plugin "0.1.0-SNAPSHOT"]
-    [gov.nasa.earthdata/cmr-metadata-proxy "0.1.2-SNAPSHOT"]
+    [gov.nasa.earthdata/cmr-metadata-proxy "0.1.3-SNAPSHOT"]
     [gov.nasa.earthdata/cmr-mission-control "0.1.0-SNAPSHOT"]
     [gov.nasa.earthdata/cmr-site-templates "0.1.0-SNAPSHOT"]
-    [metosin/ring-http-response "0.9.0"]
+    [metosin/ring-http-response "0.9.1"]
     [org.clojure/clojure "1.9.0"]
     [org.clojure/core.async "0.4.474"]
     [org.clojure/core.cache "0.7.1"]
@@ -56,7 +56,7 @@
       :source-paths ["test"]}
     :security {
       :plugins [
-        [lein-nvd "0.5.5"]]
+        [lein-nvd "0.5.6"]]
       :source-paths ^:replace ["src"]
       :nvd {
         :suppression-file "resources/security/false-positives.xml"}
@@ -174,4 +174,3 @@
       ["clean"]
       ["build-jar"]
       ["deploy" "clojars"]]})
-
